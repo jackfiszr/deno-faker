117e0b4fb1ca20efddd92d04b40106da933ff5c5
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-03-17 22:37:35 +0100

    locales tests pass

---
+++ test/locales_test.ts
@@ -0,0 +1,17 @@
+import { assertEquals } from "./support/test_deps.ts";
+import { faker } from "../mod.ts";
+const { test } = Deno;
+
+// TODO: make some tests for getting / setting locales
+
+// Remark: actual use of locales functionality is currently tested in all.functional.js test
+
+test({
+  name: "setLocale() changes faker.locale",
+  fn() {
+    for (const locale in faker.locales) {
+      faker.setLocale(locale);
+      assertEquals(faker.locale, locale);
+    }
+  }
+});
