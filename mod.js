c1ea03d56753b8f03a5a68e5694bffb932ccc1e0
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-03-17 10:41:30 +0100

    main mod es6 import/export

--- mod.js
+++ mod.js
@@ -1,4 +1,4 @@
 // since we are requiring the top level of faker, load all locales by default
-var Faker = require("./lib");
-var faker = new Faker({ locales: require("./lib/locales") });
-module["exports"] = faker;
+import * as locales from "./lib/locales.js";
+import { Faker } from "./lib/mod.js";
+export const faker = new Faker({ locales });
