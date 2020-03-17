07912f370527f4d807f0450b7a276b333475cc92
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-03-17 22:36:28 +0100

    specify test deps ver

--- test/support/test_deps.ts
+++ test/support/test_deps.ts
@@ -4,6 +4,6 @@ export {
   assertNotEquals,
   assertStrictEq,
   assertThrows
-} from "https://deno.land/std/testing/asserts.ts";
+} from "https://deno.land/std@v0.36.0/testing/asserts.ts";
 export * as sinon from "https://cdn.pika.dev/sinon@^9.0.0";
 export * as _ from "https://cdn.pika.dev/lodash-es@^4.17.15";
