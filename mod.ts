d3622f5fc4fe27dc8a922213323f25c0b2b95232
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-03-18 19:39:59 +0100

    lib\locales.js=>ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
 // since we are requiring the top level of faker, load all locales by default
-import * as locales from "./lib/locales.js";
+import * as locales from "./lib/locales.ts";
 import { Faker } from "./lib/mod.ts";
 export const faker = new Faker({ locales });
