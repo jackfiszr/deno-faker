9a7f7465a07c5f2594d334fbb5ae7fbdd350b52f
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-03-18 22:52:17 +0100

    mersenne ts redo

--- vendor/mersenne.ts
+++ vendor/mersenne.ts
@@ -49,10 +49,13 @@
    http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
    email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
 */
-
-function MersenneTwister19937() {
+declare class MersenneTwister19937 {
+  [key: string]: Function
+}
+function MersenneTwister19937(this: MersenneTwister19937) {
   /* constants should be scoped inside the class */
-  var N, M, MATRIX_A, UPPER_MASK, LOWER_MASK;
+  let N: number, M: number, MATRIX_A: number, UPPER_MASK: number,
+    LOWER_MASK: number;
   /* Period parameters */
   //c//#define N 624
   //c//#define M 397
@@ -66,30 +69,30 @@ function MersenneTwister19937() {
   LOWER_MASK = 0x7fffffff; /* least significant r bits */
   //c//static unsigned long mt[N]; /* the array for the state vector  */
   //c//static int mti=N+1; /* mti==N+1 means mt[N] is not initialized */
-  var mt = new Array(N); /* the array for the state vector  */
-  var mti = N + 1; /* mti==N+1 means mt[N] is not initialized */
+  const mt = new Array(N); /* the array for the state vector  */
+  let mti = N + 1; /* mti==N+1 means mt[N] is not initialized */
 
-  function unsigned32(n1) // returns a 32-bits unsiged integer from an operand to which applied a bit operator.
+  function unsigned32(n1: number) // returns a 32-bits unsiged integer from an operand to which applied a bit operator.
   {
     return n1 < 0 ? (n1 ^ UPPER_MASK) + UPPER_MASK : n1;
   }
 
-  function subtraction32(n1, n2) // emulates lowerflow of a c 32-bits unsiged integer variable, instead of the operator -. these both arguments must be non-negative integers expressible using unsigned 32 bits.
+  function subtraction32(n1: number, n2: number) // emulates lowerflow of a c 32-bits unsiged integer variable, instead of the operator -. these both arguments must be non-negative integers expressible using unsigned 32 bits.
   {
     return n1 < n2
       ? unsigned32((0x100000000 - (n2 - n1)) & 0xffffffff)
       : n1 - n2;
   }
 
-  function addition32(n1, n2) // emulates overflow of a c 32-bits unsiged integer variable, instead of the operator +. these both arguments must be non-negative integers expressible using unsigned 32 bits.
+  function addition32(n1: number, n2: number) // emulates overflow of a c 32-bits unsiged integer variable, instead of the operator +. these both arguments must be non-negative integers expressible using unsigned 32 bits.
   {
     return unsigned32((n1 + n2) & 0xffffffff);
   }
 
-  function multiplication32(n1, n2) // emulates overflow of a c 32-bits unsiged integer variable, instead of the operator *. these both arguments must be non-negative integers expressible using unsigned 32 bits.
+  function multiplication32(n1: number, n2: number) // emulates overflow of a c 32-bits unsiged integer variable, instead of the operator *. these both arguments must be non-negative integers expressible using unsigned 32 bits.
   {
-    var sum = 0;
-    for (var i = 0; i < 32; ++i) {
+    let sum = 0;
+    for (let i = 0; i < 32; ++i) {
       if ((n1 >>> i) & 0x1) {
         sum = addition32(sum, unsigned32(n2 << i));
       }
@@ -99,7 +102,7 @@ function MersenneTwister19937() {
 
   /* initializes mt[N] with a seed */
   //c//void init_genrand(unsigned long s)
-  this.init_genrand = function(s) {
+  this.init_genrand = function(s: number) {
     //c//mt[0]= s & 0xffffffff;
     mt[0] = unsigned32(s & 0xffffffff);
     for (mti = 1; mti < N; mti++) {
@@ -126,9 +129,9 @@ function MersenneTwister19937() {
   /* key_length is its length */
   /* slight change for C++, 2004/2/26 */
   //c//void init_by_array(unsigned long init_key[], int key_length)
-  this.init_by_array = function(init_key, key_length) {
+  this.init_by_array = function(init_key: number[], key_length: number) {
     //c//int i, j, k;
-    var i, j, k;
+    let i, j, k;
     //c//init_genrand(19650218);
     this.init_genrand(19650218);
     i = 1;
@@ -164,7 +167,7 @@ function MersenneTwister19937() {
       //c//- i; /* non linear */
       mt[i] = subtraction32(
         unsigned32(
-          mt[i] ^ multiplication32(
+          (mt[i]) ^ multiplication32(
             unsigned32(mt[i - 1] ^ (mt[i - 1] >>> 30)),
             1566083941
           )
@@ -183,20 +186,20 @@ function MersenneTwister19937() {
   };
 
   /* moved outside of genrand_int32() by jwatte 2010-11-17; generate less garbage */
-  var mag01 = [0x0, MATRIX_A];
+  const mag01 = [0x0, MATRIX_A];
 
   /* generates a random number on [0,0xffffffff]-interval */
   //c//unsigned long genrand_int32(void)
   this.genrand_int32 = function() {
     //c//unsigned long y;
     //c//static unsigned long mag01[2]={0x0UL, MATRIX_A};
-    var y;
+    let y;
     /* mag01[x] = x * MATRIX_A  for x=0,1 */
 
     if (mti >= N) {
       /* generate N words at one time */
       //c//int kk;
-      var kk;
+      let kk;
 
       if (mti == N + 1) {
         /* if init_genrand() has not been called, */
@@ -273,36 +276,39 @@ function MersenneTwister19937() {
   //c//double genrand_res53(void)
   this.genrand_res53 = function() {
     //c//unsigned long a=genrand_int32()>>5, b=genrand_int32()>>6;
-    var a = this.genrand_int32() >>> 5, b = this.genrand_int32() >>> 6;
+    const a = this.genrand_int32() >>> 5, b = this.genrand_int32() >>> 6;
     return (a * 67108864.0 + b) * (1.0 / 9007199254740992.0);
   };
   /* These real versions are due to Isaku Wada, 2002/01/09 added */
 }
 
-//  Exports: Public API
+//  export const  Public API
 
 //  Export the twister class
 export { MersenneTwister19937 };
 
 //  Export a simplified function to generate random numbers
-var gen = new MersenneTwister19937();
+const gen = new MersenneTwister19937();
 gen.init_genrand((new Date()).getTime() % 1000000000);
 
 // Added max, min range functionality, Marak Squires Sept 11 2014
-export const rand = function(max, min) {
+export const rand = function(max?: number, min?: number) {
   if (max === undefined) {
     min = 0;
     max = 32768;
   }
+  if (min === undefined) {
+    min = 0;
+  }
   return Math.floor(gen.genrand_real2() * (max - min) + min);
 };
-export const seed = function(S) {
+export const seed = function(S: number) {
   if (typeof (S) != "number") {
     throw new Error("seed(S) must take numeric argument; is " + typeof (S));
   }
   gen.init_genrand(S);
 };
-export const seed_array = function(A) {
+export const seed_array = function(A: number[]) {
   if (typeof (A) != "object") {
     throw new Error(
       "seed_array(A) must take array of numbers; is " + typeof (A)
