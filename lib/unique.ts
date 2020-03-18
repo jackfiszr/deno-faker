58a23ead6cacd2ed0ff69ee7910b93e0b392607c
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-03-18 22:16:13 +0100

    lib/unique redone in ts

--- lib/unique.ts
+++ lib/unique.ts
@@ -3,35 +3,36 @@ import uniqueExec from "../vendor/unique.js";
  *
  * @namespace faker.unique
  */
-function Unique(faker) {
+class Unique {
+  constructor(faker: any) {}
   // initialize unique module class variables
 
   // maximum time unique.exec will attempt to run before aborting
-  var maxTime = 10;
+  maxTime?: 10;
 
   // maximum retries unique.exec will recurse before abortings ( max loop depth )
-  var maxRetries = 10;
+  maxRetries?: 10;
 
   // time the script started
-  // var startTime = 0;
+  // const startTime = 0;
 
   /**
    * unique
    *
    * @method unique
    */
-  this.unique = function unique(method, args, opts) {
+  unique(method: Function, args: any, opts: any) {
     opts = opts || {};
     opts.startTime = new Date().getTime();
     if (typeof opts.maxTime !== "number") {
-      opts.maxTime = maxTime;
+      opts.maxTime = this.maxTime;
     }
     if (typeof opts.maxRetries !== "number") {
-      opts.maxRetries = maxRetries;
+      opts.maxRetries = this.maxRetries;
     }
     opts.currentIterations = 0;
     return uniqueExec.exec(method, args, opts);
-  };
+  }
 }
 
 export { Unique };
