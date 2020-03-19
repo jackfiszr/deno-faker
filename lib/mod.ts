e99a24541392abf23db404e900cfd509b12988d1
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-03-19 16:28:09 +0100

    lib/lorem.js=>ts

--- lib/mod.ts
+++ lib/mod.ts
@@ -25,7 +25,7 @@ import { Address } from "./address.ts";
 import { Company } from "./company.ts";
 import { Finance } from "./finance.ts";
 import { Image } from "./image.ts";
-import { Lorem } from "./lorem.js";
+import { Lorem } from "./lorem.ts";
 import { Hacker } from "./hacker.js";
 import { Internet } from "./internet.js";
 import { Database } from "./database.js";
