71334bfdf8666d2b5b106ebd09fe70966d751bcd
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-06-25 13:17:30 +0200

    migrate to jspm for sinon

--- test/support/test_deps.ts
+++ test/support/test_deps.ts
@@ -6,6 +6,6 @@ export {
   assertThrows
 } from "https://deno.land/std@0.58.0/testing/asserts.ts";
 // @ts-ignore
-export * as sinon from "https://cdn.pika.dev/sinon@^9.0.2";
+export { default as sinon } from "https://dev.jspm.io/sinon@9.0.2";
 // @ts-ignore
 export * as _ from "https://cdn.pika.dev/lodash-es@^4.17.15";
