716b28c23db5b7d15a79e6416723a0ab5d2f3057
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-12-27 15:01:45 +0100

    test(deps): update to std@0.82.0

--- test/support/test_deps.ts
+++ test/support/test_deps.ts
@@ -4,7 +4,7 @@ export {
   assertNotEquals,
   assertStrictEquals,
   assertThrows
-} from "https://deno.land/std@0.58.0/testing/asserts.ts";
+} from "https://deno.land/std@0.82.0/testing/asserts.ts";
 // @ts-ignore
 export { default as sinon } from "https://dev.jspm.io/sinon@9.0.2";
 // @ts-ignore
