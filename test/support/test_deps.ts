b99720bff58b746ad7a260bf237594a09c45f683
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-12-27 15:29:42 +0100

    test(deps): update to lodash-es@^4.17.20

--- test/support/test_deps.ts
+++ test/support/test_deps.ts
@@ -8,4 +8,4 @@ export {
 // @ts-ignore
 export { default as sinon } from "https://cdn.skypack.dev/sinon@9.2.2";
 // @ts-ignore
-export * as _ from "https://cdn.pika.dev/lodash-es@^4.17.15";
+export * as _ from "https://cdn.skypack.dev/lodash-es@^4.17.20";
