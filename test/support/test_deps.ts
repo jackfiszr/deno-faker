c558028068baf15abe5d1e49f045e73fba00dfcd
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-12-27 15:18:05 +0100

    test(deps): update to sinon@9.2.2

--- test/support/test_deps.ts
+++ test/support/test_deps.ts
@@ -6,6 +6,6 @@ export {
   assertThrows
 } from "https://deno.land/std@0.82.0/testing/asserts.ts";
 // @ts-ignore
-export { default as sinon } from "https://dev.jspm.io/sinon@9.0.2";
+export { default as sinon } from "https://cdn.skypack.dev/sinon@9.2.2";
 // @ts-ignore
 export * as _ from "https://cdn.pika.dev/lodash-es@^4.17.15";
