b218bbb7313e1cff2fc88627a97167757b02f04a
Jacek Fiszer <jackfiszr+gh@gmail.com>
2021-01-09 16:26:51 +0100

    fix(test_deps): migrate to deno.land/x lodash (downgrade to 4.17.19)

--- test/support/test_deps.ts
+++ test/support/test_deps.ts
@@ -8,4 +8,4 @@ export {
 // @ts-ignore
 export { default as sinon } from "https://cdn.skypack.dev/sinon@9.2.3";
 // @ts-ignore
-export * as _ from "https://cdn.skypack.dev/lodash-es@^4.17.20";
+export * as _ from "https://deno.land/x/lodash@4.17.19/lodash.js";
