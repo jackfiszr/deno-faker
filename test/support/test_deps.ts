b98bdb6df58fac67e468b5e37c94741629663849
Jacek Fiszer <jackfiszr+gh@gmail.com>
2021-01-09 16:30:36 +0100

    refactor: remove @ts-ignore from test deps

--- test/support/test_deps.ts
+++ test/support/test_deps.ts
@@ -5,7 +5,5 @@ export {
   assertStrictEquals,
   assertThrows,
 } from "https://deno.land/std@0.83.0/testing/asserts.ts";
-// @ts-ignore
 export { default as sinon } from "https://cdn.skypack.dev/sinon@9.2.3";
-// @ts-ignore
 export * as _ from "https://deno.land/x/lodash@4.17.19/lodash.js";
