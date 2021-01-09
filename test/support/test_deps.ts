5e5e064642b12af2ec9d3638a2cead6e145e7396
Jacek Fiszer <jackfiszr+gh@gmail.com>
2021-01-09 16:11:48 +0100

    fix(test_deps): update to sinon 9.2.3

--- test/support/test_deps.ts
+++ test/support/test_deps.ts
@@ -6,6 +6,6 @@ export {
   assertThrows,
 } from "https://deno.land/std@0.83.0/testing/asserts.ts";
 // @ts-ignore
-export { default as sinon } from "https://cdn.skypack.dev/sinon@9.2.2";
+export { default as sinon } from "https://cdn.skypack.dev/sinon@9.2.3";
 // @ts-ignore
 export * as _ from "https://cdn.skypack.dev/lodash-es@^4.17.20";
