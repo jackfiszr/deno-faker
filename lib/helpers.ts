3070a9107fd05667bb31d99abd345321afaac573
Jacek Fiszer <jackfiszr+gh@gmail.com>
2024-12-15 19:09:24 +0100

    refactor(lib): add return types for helpers

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,54 +1,51 @@
 import type { Faker } from "./mod.ts";
-/**
- * @namespace faker.helpers
- */
+
 class Helpers {
   faker: Faker;
+
   constructor(faker: Faker) {
     this.faker = faker;
   }
 
   /**
-   * backword-compatibility
+   * Randomizes an array and returns a random element.
    *
    * @method faker.helpers.randomize
-   * @param {array} array
+   * @param {string[]} array
+   * @returns {string}
    */
-  randomize(array: string[]) {
+  randomize(array: string[]): string {
     array = array || ["a", "b", "c"];
     return this.faker.random.arrayElement(array);
   }
 
   /**
-   * slugifies string
+   * Slugifies a string.
    *
    * @method faker.helpers.slugify
    * @param {string} string
+   * @returns {string}
    */
-  slugify(string: string) {
+  slugify(string: string): string {
     string = string || "";
     return string.replace(/ /g, "-").replace(/[^\w\.\-]+/g, "");
   }
 
   /**
-   * parses string for a symbol and replace it with a random number from 1-10
+   * Replaces symbols in a string with random numbers.
    *
    * @method faker.helpers.replaceSymbolWithNumber
    * @param {string} string
-   * @param {string} symbol defaults to `"#"`
+   * @param {string} symbol
+   * @returns {string}
    */
-  replaceSymbolWithNumber(string: string, symbol?: string) {
+  replaceSymbolWithNumber(string: string, symbol: string = "#"): string {
     string = string || "";
-    // default symbol is '#'
-    if (symbol === undefined) {
-      symbol = "#";
-    }
-
     let str = "";
     for (let i = 0; i < string.length; i++) {
-      if (string.charAt(i) == symbol) {
+      if (string.charAt(i) === symbol) {
         str += this.faker.random.number(9);
-      } else if (string.charAt(i) == "!") {
+      } else if (string.charAt(i) === "!") {
         str += this.faker.random.number({ min: 2, max: 9 });
       } else {
         str += string.charAt(i);
@@ -58,50 +55,23 @@ class Helpers {
   }
 
   /**
-   * parses string for symbols (numbers or letters) and replaces them appropriately (# will be replaced with number,
-   * ? with letter and * will be replaced with number or letter)
+   * Replaces symbols (#, ?, *) in a string with numbers, letters, or both.
    *
    * @method faker.helpers.replaceSymbols
    * @param {string} string
+   * @returns {string}
    */
-  replaceSymbols(string: string) {
+  replaceSymbols(string: string): string {
     string = string || "";
-    const alpha = [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-    ];
+    const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     let str = "";
 
     for (let i = 0; i < string.length; i++) {
-      if (string.charAt(i) == "#") {
+      if (string.charAt(i) === "#") {
         str += this.faker.random.number(9);
-      } else if (string.charAt(i) == "?") {
+      } else if (string.charAt(i) === "?") {
         str += this.faker.random.arrayElement(alpha);
-      } else if (string.charAt(i) == "*") {
+      } else if (string.charAt(i) === "*") {
         str += this.faker.random.boolean()
           ? this.faker.random.arrayElement(alpha)
           : this.faker.random.number(9);
@@ -113,20 +83,17 @@ class Helpers {
   }
 
   /**
-   * replace symbols in a credit card schems including Luhn checksum
+   * Replaces credit card symbols and calculates the Luhn checksum.
    *
    * @method faker.helpers.replaceCreditCardSymbols
    * @param {string} string
    * @param {string} symbol
+   * @returns {string}
    */
-
-  replaceCreditCardSymbols = (string: string, symbol?: string) => {
-    symbol = symbol || "#";
-
-    // Function calculating the Luhn checksum of a number string
-    const getCheckBit = (number: number[]) => {
+  replaceCreditCardSymbols = (string: string, symbol: string = "#"): string => {
+    const getCheckBit = (number: number[]): number => {
       number.reverse();
-      number = number.map(function (num, index) {
+      number = number.map((num, index) => {
         if (index % 2 === 0) {
           num *= 2;
           if (num > 9) {
@@ -135,30 +102,28 @@ class Helpers {
         }
         return num;
       });
-      const sum = number.reduce(function (prev, curr) {
-        return prev + curr;
-      });
+      const sum = number.reduce((prev, curr) => prev + curr, 0);
       return sum % 10;
     };
 
     string = string || "";
-    string = this.faker.helpers.regexpStyleStringParse(string); // replace [4-9] with a random number in range etc...
-    string = this.faker.helpers.replaceSymbolWithNumber(string, symbol); // replace ### with random numbers
+    string = this.faker.helpers.regexpStyleStringParse(string);
+    string = this.faker.helpers.replaceSymbolWithNumber(string, symbol);
 
-    const numberList = string.replace(/\D/g, "").split("").map(function (num) {
-      return parseInt(num);
-    });
+    const numberList = string.replace(/\D/g, "").split("").map(Number);
     const checkNum = getCheckBit(numberList);
     return string.replace("L", String(checkNum));
   };
 
-  /** string repeat helper, alternative to String.prototype.repeat.... See PR #382
+  /**
+   * Repeats a string a specified number of times.
    *
    * @method faker.helpers.repeatString
    * @param {string} string
    * @param {number} num
+   * @returns {string}
    */
-  repeatString = (string: string, num: number) => {
+  repeatString = (string: string, num: number): string => {
     if (typeof num === "undefined") {
       num = 0;
     }
@@ -170,37 +135,35 @@ class Helpers {
   };
 
   /**
-   * parse string paterns in a similar way to RegExp
-   *
-   * e.g. "#{3}test[1-5]" -> "###test4"
+   * Parses string patterns in a RegExp-like manner.
    *
    * @method faker.helpers.regexpStyleStringParse
    * @param {string} string
+   * @returns {string}
    */
-  regexpStyleStringParse = (string: string) => {
+  regexpStyleStringParse = (string: string): string => {
     string = string || "";
-    // Deal with range repeat `{min,max}`
-    const RANGE_REP_REG = /(.)\{(\d+)\,(\d+)\}/;
+    const RANGE_REP_REG = /(.)\{(\d+),(\d+)\}/;
     const REP_REG = /(.)\{(\d+)\}/;
-    const RANGE_REG = /\[(\d+)\-(\d+)\]/;
+    const RANGE_REG = /\[(\d+)-(\d+)\]/;
     let min, max, tmp, repetitions;
     let token = string.match(RANGE_REP_REG);
+
     while (token !== null && token.index !== undefined) {
       min = parseInt(token[2]);
       max = parseInt(token[3]);
-      // switch min and max
       if (min > max) {
         tmp = max;
         max = min;
         min = tmp;
       }
-      repetitions = this.faker.random.number({ min: min, max: max });
+      repetitions = this.faker.random.number({ min, max });
       string = string.slice(0, token.index) +
         this.faker.helpers.repeatString(token[1], repetitions) +
         string.slice(token.index + token[0].length);
       token = string.match(RANGE_REP_REG);
     }
-    // Deal with repeat `{num}`
+
     token = string.match(REP_REG);
     while (token !== null && token.index !== undefined) {
       repetitions = parseInt(token[2]);
@@ -209,34 +172,33 @@ class Helpers {
         string.slice(token.index + token[0].length);
       token = string.match(REP_REG);
     }
-    // Deal with range `[min-max]` (only works with numbers for now)
-    //TODO: implement for letters e.g. [0-9a-zA-Z] etc.
 
     token = string.match(RANGE_REG);
     while (token !== null && token.index !== undefined) {
-      min = parseInt(token[1]); // This time we are not capturing the char befor `[]`
+      min = parseInt(token[1]);
       max = parseInt(token[2]);
-      // switch min and max
       if (min > max) {
         tmp = max;
         max = min;
         min = tmp;
       }
       string = string.slice(0, token.index) +
-        this.faker.random.number({ min: min, max: max }).toString() +
+        this.faker.random.number({ min, max }).toString() +
         string.slice(token.index + token[0].length);
       token = string.match(RANGE_REG);
     }
+
     return string;
   };
 
   /**
-   * takes an array and returns it randomized
+   * Shuffles an array and returns it.
    *
    * @method faker.helpers.shuffle
-   * @param {array} o
+   * @param {string[]} o
+   * @returns {string[]}
    */
-  shuffle(o: string[]) {
+  shuffle(o: string[]): string[] {
     if (typeof o === "undefined" || o.length === 0) {
       return [];
     }
@@ -250,13 +212,14 @@ class Helpers {
   }
 
   /**
-   * mustache
+   * Parses a mustache template with data.
    *
    * @method faker.helpers.mustache
    * @param {string} str
-   * @param {object} data
+   * @param {Record<string, unknown>} data
+   * @returns {string}
    */
-  mustache(str: string, data: Record<string, unknown>) {
+  mustache(str: string, data: Record<string, unknown>): string {
     if (typeof str === "undefined") {
       return "";
     }
@@ -269,57 +232,58 @@ class Helpers {
   }
 
   /**
-   * createCard
+   * Creates a random user card object.
    *
    * @method faker.helpers.createCard
+   * @returns {Record<string, unknown>}
    */
-  createCard() {
+  createCard(): Record<string, unknown> {
     return {
-      "name": this.faker.name.findName(),
-      "username": this.faker.internet.userName(),
-      "email": this.faker.internet.email(),
-      "address": {
-        "streetA": this.faker.address.streetName(),
-        "streetB": this.faker.address.streetAddress(),
-        "streetC": this.faker.address.streetAddress(true),
-        "streetD": this.faker.address.secondaryAddress(),
-        "city": this.faker.address.city(),
-        "state": this.faker.address.state(),
-        "country": this.faker.address.country(),
-        "zipcode": this.faker.address.zipCode(),
-        "geo": {
-          "lat": this.faker.address.latitude(),
-          "lng": this.faker.address.longitude(),
+      name: this.faker.name.findName(),
+      username: this.faker.internet.userName(),
+      email: this.faker.internet.email(),
+      address: {
+        streetA: this.faker.address.streetName(),
+        streetB: this.faker.address.streetAddress(),
+        streetC: this.faker.address.streetAddress(true),
+        streetD: this.faker.address.secondaryAddress(),
+        city: this.faker.address.city(),
+        state: this.faker.address.state(),
+        country: this.faker.address.country(),
+        zipcode: this.faker.address.zipCode(),
+        geo: {
+          lat: this.faker.address.latitude(),
+          lng: this.faker.address.longitude(),
         },
       },
-      "phone": this.faker.phone.phoneNumber(),
-      "website": this.faker.internet.domainName(),
-      "company": {
-        "name": this.faker.company.companyName(),
-        "catchPhrase": this.faker.company.catchPhrase(),
-        "bs": this.faker.company.bs(),
+      phone: this.faker.phone.phoneNumber(),
+      website: this.faker.internet.domainName(),
+      company: {
+        name: this.faker.company.companyName(),
+        catchPhrase: this.faker.company.catchPhrase(),
+        bs: this.faker.company.bs(),
       },
-      "posts": [
+      posts: [
         {
-          "words": this.faker.lorem.words(),
-          "sentence": this.faker.lorem.sentence(),
-          "sentences": this.faker.lorem.sentences(),
-          "paragraph": this.faker.lorem.paragraph(),
+          words: this.faker.lorem.words(),
+          sentence: this.faker.lorem.sentence(),
+          sentences: this.faker.lorem.sentences(),
+          paragraph: this.faker.lorem.paragraph(),
         },
         {
-          "words": this.faker.lorem.words(),
-          "sentence": this.faker.lorem.sentence(),
-          "sentences": this.faker.lorem.sentences(),
-          "paragraph": this.faker.lorem.paragraph(),
+          words: this.faker.lorem.words(),
+          sentence: this.faker.lorem.sentence(),
+          sentences: this.faker.lorem.sentences(),
+          paragraph: this.faker.lorem.paragraph(),
         },
         {
-          "words": this.faker.lorem.words(),
-          "sentence": this.faker.lorem.sentence(),
-          "sentences": this.faker.lorem.sentences(),
-          "paragraph": this.faker.lorem.paragraph(),
+          words: this.faker.lorem.words(),
+          sentence: this.faker.lorem.sentence(),
+          sentences: this.faker.lorem.sentences(),
+          paragraph: this.faker.lorem.paragraph(),
         },
       ],
-      "accountHistory": [
+      accountHistory: [
         this.faker.helpers.createTransaction(),
         this.faker.helpers.createTransaction(),
         this.faker.helpers.createTransaction(),
@@ -328,94 +292,92 @@ class Helpers {
   }
 
   /**
-   * contextualCard
+   * Creates a contextual card object.
    *
    * @method faker.helpers.contextualCard
+   * @returns {Record<string, unknown>}
    */
-  contextualCard() {
-    const name = this.faker.name.firstName(),
-      userName = this.faker.internet.userName(name);
+  contextualCard(): Record<string, unknown> {
+    const name = this.faker.name.firstName();
+    const userName = this.faker.internet.userName(name);
     return {
-      "name": name,
-      "username": userName,
-      "avatar": this.faker.internet.avatar(),
-      "email": this.faker.internet.email(userName),
-      "dob": this.faker.date.past(
+      name,
+      username: userName,
+      avatar: this.faker.internet.avatar(),
+      email: this.faker.internet.email(userName),
+      dob: this.faker.date.past(
         50,
         new Date("Sat Sep 20 1992 21:35:02 GMT+0200 (CEST)").toISOString(),
       ),
-      "phone": this.faker.phone.phoneNumber(),
-      "address": {
-        "street": this.faker.address.streetName(/* true */),
-        "suite": this.faker.address.secondaryAddress(),
-        "city": this.faker.address.city(),
-        "zipcode": this.faker.address.zipCode(),
-        "geo": {
-          "lat": this.faker.address.latitude(),
-          "lng": this.faker.address.longitude(),
+      phone: this.faker.phone.phoneNumber(),
+      address: {
+        street: this.faker.address.streetName(),
+        suite: this.faker.address.secondaryAddress(),
+        city: this.faker.address.city(),
+        zipcode: this.faker.address.zipCode(),
+        geo: {
+          lat: this.faker.address.latitude(),
+          lng: this.faker.address.longitude(),
         },
       },
-      "website": this.faker.internet.domainName(),
-      "company": {
-        "name": this.faker.company.companyName(),
-        "catchPhrase": this.faker.company.catchPhrase(),
-        "bs": this.faker.company.bs(),
+      website: this.faker.internet.domainName(),
+      company: {
+        name: this.faker.company.companyName(),
+        catchPhrase: this.faker.company.catchPhrase(),
+        bs: this.faker.company.bs(),
       },
     };
   }
 
   /**
-   * userCard
+   * Creates a user card object.
    *
    * @method faker.helpers.userCard
+   * @returns {Record<string, unknown>}
    */
-  userCard() {
+  userCard(): Record<string, unknown> {
     return {
-      "name": this.faker.name.findName(),
-      "username": this.faker.internet.userName(),
-      "email": this.faker.internet.email(),
-      "address": {
-        "street": this.faker.address.streetName(/* true */),
-        "suite": this.faker.address.secondaryAddress(),
-        "city": this.faker.address.city(),
-        "zipcode": this.faker.address.zipCode(),
-        "geo": {
-          "lat": this.faker.address.latitude(),
-          "lng": this.faker.address.longitude(),
+      name: this.faker.name.findName(),
+      username: this.faker.internet.userName(),
+      email: this.faker.internet.email(),
+      address: {
+        street: this.faker.address.streetName(),
+        suite: this.faker.address.secondaryAddress(),
+        city: this.faker.address.city(),
+        zipcode: this.faker.address.zipCode(),
+        geo: {
+          lat: this.faker.address.latitude(),
+          lng: this.faker.address.longitude(),
         },
       },
-      "phone": this.faker.phone.phoneNumber(),
-      "website": this.faker.internet.domainName(),
-      "company": {
-        "name": this.faker.company.companyName(),
-        "catchPhrase": this.faker.company.catchPhrase(),
-        "bs": this.faker.company.bs(),
+      phone: this.faker.phone.phoneNumber(),
+      website: this.faker.internet.domainName(),
+      company: {
+        name: this.faker.company.companyName(),
+        catchPhrase: this.faker.company.catchPhrase(),
+        bs: this.faker.company.bs(),
       },
     };
   }
 
   /**
-   * createTransaction
+   * Creates a random transaction object.
    *
    * @method faker.helpers.createTransaction
+   * @returns {Record<string, unknown>}
    */
-  createTransaction = () => {
+  createTransaction(): Record<string, unknown> {
     return {
-      "amount": this.faker.finance.amount(),
-      "date": new Date(2012, 1, 2), //TODO: add a ranged date method
-      "business": this.faker.company.companyName(),
-      "name": [this.faker.finance.accountName(), this.faker.finance.mask()]
-        .join(" "),
-      "type": this.randomize(this.faker.definitions.finance.transaction_type),
-      "account": this.faker.finance.account(),
+      amount: this.faker.finance.amount(),
+      date: new Date(2012, 1, 2),
+      business: this.faker.company.companyName(),
+      name: [this.faker.finance.accountName(), this.faker.finance.mask()].join(
+        " ",
+      ),
+      type: this.randomize(this.faker.definitions.finance.transaction_type),
+      account: this.faker.finance.account(),
     };
-  };
+  }
 }
-/*
-String.prototype.capitalize () { //v1.0
-  return this.replace(/\w+/g, function (a) {
-    return a.charAt(0).toUpperCase() + a.substr(1).toLowerCase();
-  });
-}; */
 
 export { Helpers };
